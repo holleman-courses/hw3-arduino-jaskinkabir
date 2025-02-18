5842e9badcd8f62e89de6a474f863f8639198306
jaskinkabir <jaskinkabir@gmail.com>
2025-02-18 09:20:53 -0500

    Quantized model

--- model_conversion/converter.py
+++ model_conversion/converter.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 path = "/home/jaskin/Intro_IoT_ML/hw3-arduino-jaskinkabir/model_conversion/sin_predictor.h5"
 
@@ -10,10 +11,20 @@ model = tf.keras.models.load_model(path)
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 
-tflite_model = converter.convert()
+rng = tf.random.Generator.from_seed(0)
+def representative_dataset():
+    for _ in range(100):
+        yield [*rng.uniform((1, 7), -1, 1)]
+        
+converter.representative_dataset = representative_dataset
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.int8
+converter.inference_output_type = tf.int8
 
+tflite_qaunt_model = converter.convert()
 
+ 
 
 # Save the TFLite model
 with open("sin_predictor.tflite", "wb") as f:
-    f.write(tflite_model)
+    f.write(tflite_qaunt_model)
